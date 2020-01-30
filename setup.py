31c2bd0e21d8e283d3227dd065fe560be97b9149
atlasd <datlas2@jhu.edu>
2020-01-29 20:37:08 -0500

    Adding requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup
 
+test = ["pytest"]
+
 setup(
     name="machine_learning",
     package_dir={"machine_learning": "machine_learning"},
     version="0.0.1",
+    extra_requires={"test": test},
 )
