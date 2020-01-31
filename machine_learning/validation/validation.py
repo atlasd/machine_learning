7c8e45aca91576990bb50032364001dc4475b31e
atlasd <datlas2@jhu.edu>
2020-01-30 21:57:54 -0500

    Adding in more test

---
+++ machine_learning/validation/validation.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+
+
+class KFoldCV:
+    def __init__(self, num_folds, shuffle=True):
+        self.num_folds = num_folds
+        self.shuffle = shuffle
+
+    def get_indices(self, X, y=None):
+        # Get indices of length rows of X. Shuffle if `self.shuffle` is true.
+        nrows = X.shape[0]
+        return np.random.shuffle(np.arange(nrows)) if self.shuffle else np.arange(nrows)
+
+    @staticmethod
+    def _get_one_split(indices, num_split):
+        return np.delete(indices, indices[num_split]), indices[num_split]
+
+    @staticmethod
+    def _get_indices_split(indices, num_folds):
+        return np.array_split(indices, indices_or_sections=num_folds)
+
+    def split(self, X, y=None):
+        # Split the indices into `num_folds` subarray
+        indices = np.split(self.get_indices(X, y), indices_or_sections=self.num_folds)
+        for num_split in self.num_folds:
+            # Return all but one split as train, and one split as test
+            yield np.delete(indices, indices[num_split]), indices[num_split]
---
+++ tests/validation/test_validation.py
@@ -0,0 +1,35 @@
+import pytest
+import numpy as np
+from machine_learning import validation
+
+
+@pytest.fixture
+def X():
+    yield np.ones((100, 10))
+
+
+@pytest.mark.parametrize(
+    "num_folds, shuffle, expected",
+    [(10, False, np.arange(100)), (10, True, np.arange(100, 0, -1)),],
+)
+def test_get_indices(X, num_folds, shuffle, expected, monkeypatch):
+    monkeypatch.setattr(
+        np.random, "shuffle", lambda *args, **kwargs: np.arange(100, 0, -1)
+    )
+    assert np.array_equal(
+        validation.KFoldCV(num_folds=num_folds, shuffle=shuffle).get_indices(X=X),
+        expected,
+    )
+
+
+@pytest.mark.parametrize(
+    "indices, num_folds, expected",
+    [
+        (np.arange(10), 2, [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]),
+        (np.arange(10), 3, [[0, 1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+    ],
+)
+def test_get_one_split(indices, num_folds, expected):
+    out = validation.KFoldCV._get_indices_split(indices, num_folds=num_folds)
+    for out_arr, expt_arr in zip(out, expected):
+        assert np.allclose(out_arr, expt_arr)
