268ebd7d2c5836c5c06f11cb922b1422243c6904
atlasd <datlas2@jhu.edu>
2020-02-26 20:13:35 -0500

    adding in final dtree

--- machine_learning/trees.py
+++ machine_learning/trees.py
@@ -14,6 +14,20 @@ def uncertainty(y):
 
 
 class TreeSplits:
+    def __init__(
+        self,
+        feature_col=None,
+        feature_value=None,
+        node_type=None,
+        nodes=None,
+        children=[],
+    ):
+        self.feature_col = feature_col
+        self.feature_value = feature_value
+        self.node_type = node_type
+        self.nodes = nodes
+        self.children = children
+
     def update(self, feature_col, feature_value, node_type, nodes, children=[]):
         self.feature_col = feature_col
         self.feature_value = feature_value
@@ -30,6 +44,7 @@ class BaseTree:
         self.early_stopping_value = (
             0 if not early_stopping_value else early_stopping_value
         )
+        self.n_nodes = 1
 
     @staticmethod
     def get_unique_sorted(arr):
@@ -80,6 +95,53 @@ class BaseTree:
             X[:, feature_col], y, eval_func=self.eval_func
         )
 
+    def get_terminal_node(self, feature_col, node, feature_value, X, y):
+        node_type = self.col_type_map[feature_col]
+        if node_type == "continuous":
+            above = X[:, feature_col] > feature_value
+            node.update(
+                feature_col=feature_col,
+                feature_value=feature_value,
+                node_type=node_type,
+                nodes={
+                    "above": TreeSplits(children=y[above]),
+                    "below": TreeSplits(children=y[np.bitwise_not(above)]),
+                },
+            )
+            self.n_nodes += 2
+        else:
+            unique_x_vals = set(X[:, feature_col])
+            node.update(
+                feature_col=feature_col,
+                feature_value=None,
+                nodes={
+                    xval: TreeSplits(children=y[X[:, feature_col] == xval])
+                    for xval in unique_x_vals
+                },
+                node_type="discrete",
+            )
+            self.n_nodes += len(unique_x_vals)
+
+    def get_continuous_node(self, feature_col, feature_value, X, y, node):
+        node.update(
+            feature_col=feature_col,
+            feature_value=feature_value,
+            nodes={"below": TreeSplits(), "above": TreeSplits()},
+            node_type="continuous",
+        )
+
+        above = X[:, feature_col] >= feature_value
+        self.get_next_split(X=X[above], y=y[above], tree_split=node.nodes["above"])
+        self.n_nodes += 1
+        self.get_next_split(
+            X=X[np.bitwise_not(above)],
+            y=y[np.bitwise_not(above)],
+            tree_split=node.nodes["below"],
+        )
+        self.n_nodes += 1
+
+        return node
+
     def get_next_split(self, X, y, tree_split):
         column_values = {}
 
@@ -111,42 +173,29 @@ class BaseTree:
             column_values, key=lambda x: column_values.get(x)[1]
         )
 
-        print(column_values.get(col_idx_with_min_value)[1], self.early_stopping_value)
-        if column_values.get(col_idx_with_min_value)[1] <= self.early_stopping_value:
-            tree_split.update(
+        if (
+            column_values.get(col_idx_with_min_value)[1] <= self.early_stopping_value
+        ) or not np.isfinite(column_values.get(col_idx_with_min_value)[1]):
+            self.get_terminal_node(
                 feature_col=col_idx_with_min_value,
                 feature_value=column_values[col_idx_with_min_value][0],
-                nodes={},
-                node_type=None,
-                children=y,
+                node=tree_split,
+                X=X,
+                y=y,
             )
             return tree_split
 
         if self.col_type_map.get(col_idx_with_min_value) == "continuous":
-            tree_split.update(
+            return self.get_continuous_node(
                 feature_col=col_idx_with_min_value,
                 feature_value=column_values[col_idx_with_min_value][0],
-                nodes={"below": TreeSplits(), "above": TreeSplits()},
-                node_type="continuous",
+                X=X,
+                y=y,
+                node=tree_split,
             )
 
-            above = (
-                X[:, col_idx_with_min_value] >= column_values[col_idx_with_min_value][0]
-            )
-
-            print("Adding a right node")
-            self.get_next_split(
-                X=X[above], y=y[above], tree_split=tree_split.nodes["above"]
-            )
-            print("Adding a left node")
-            self.get_next_split(
-                X=X[np.bitwise_not(above)],
-                y=y[np.bitwise_not(above)],
-                tree_split=tree_split.nodes["below"],
-            )
-            return tree_split
         else:
-            unique_x_vals = set(X[:col_idx_with_min_value])
+            unique_x_vals = set(X[:, col_idx_with_min_value])
             tree_split.update(
                 feature_col=col_idx_with_min_value,
                 feature_value=column_values[col_idx_with_min_value][0],
@@ -154,11 +203,12 @@ class BaseTree:
                 node_type="discrete",
             )
 
-            for idx, x_col_value in unique_x_vals:
+            for x_col_value in unique_x_vals:
                 matches = X[:, col_idx_with_min_value] == x_col_value
                 self.get_next_split(
                     X=X[matches], y=y[matches], tree_split=tree_split.nodes[x_col_value]
                 )
+                self.n_nodes += 1
 
             return tree_split
 
@@ -167,7 +217,7 @@ class BaseTree:
         self.get_next_split(X=X, y=y, tree_split=self.root)
 
     def predict_node(self, X, node):
-        if node.children:
+        if node.children is not None and len(node.children):
             return self.agg_func(node.children)
 
         if node.node_type == "continuous":
@@ -175,12 +225,12 @@ class BaseTree:
                 return self.predict_node(X=X, node=node.nodes["above"])
             else:
                 return self.predict_node(X=X, node=node.nodes["below"])
-        import ipdb
 
-        ipdb.set_trace()
         return self.predict_node(X=X, node=node.nodes[X[node.feature_col]])
 
     def predict(self, X):
+        if X.ndim == 1:
+            return self.predict_node(X=X, node=self.root)
 
         return [self.predict_node(X=row, node=self.root) for row in X]
 
@@ -191,5 +241,15 @@ class DecisionTreeClassifier(BaseTree):
             col_type_map=col_type_map,
             eval_func=eval_func,
             early_stopping_value=early_stopping_value,
-            agg_func=lambda y: Counter(y).most_common(1),
+            agg_func=lambda y: Counter(y).most_common(1)[0][0],
+        )
+
+
+class DecisionTreeRegressor(BaseTree):
+    def __init__(self, col_type_map, eval_func, early_stopping_value=None):
+        super().__init__(
+            col_type_map=col_type_map,
+            eval_func=eval_func,
+            early_stopping_value=early_stopping_value,
+            agg_func=lambda y: np.mean(y),
         )
